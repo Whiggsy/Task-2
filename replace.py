a25635ea6f96e9b8ef13ff0060d490cc69018740
Whiggsy <128292907+Whiggsy@users.noreply.github.com>
2023-03-23 00:29:44 +0000

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,5 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!."
+sentence_replace = sentence.replace("!", " ")
+print(sentence_replace)
+print(sentence_replace.upper())
+print(sentence_replace[::-1])
